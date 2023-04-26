3f754c92a43567e72cd5e3cd567fe81342c7da3d
Atul907 <atulmishra907@gmail.com>
2023-04-26 20:38:45 +0530

    rev

---
+++ JavaRevision/src/methods/MainMethod.java
@@ -0,0 +1,16 @@
+package methods;
+
+public class MainMethod {
+
+	
+	
+// access_modifier method_type return_type method_name (String array argument)
+// argument can be changed args = atul
+	
+	public static void main(String [] atul) {
+		
+		
+	System.out.println("Main method predefined can not change wording/arrangement or will throw error");
+	}
+	
+}
